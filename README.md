3bd001acd66af8e94ebc2bd7a034d7c71b0cbc0f
FBDev64 <elzadam11@gmail.com>
2024-05-02 16:08:42 +0000

    Added everything

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sph2-rs
+
+## C'est quoi ?
+Une nouvelle version qui utilise un module pour calculer les valeures.
+
+## New
+On peut désormais utiliser ```cargo run 7.25``` et remplacer 7.25 par la valeure voulue en tant que rayon.
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+mod calcul;
+use std::env;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let rayon_str = &args[1];
+	let rayon = rayon_str.parse::<f64>().unwrap();
+
+	let p = calcul::perimetre_cercle(rayon);
+	println!(">>> Le périmètre su cercle est de : {}", p);
+
+	let s = calcul::surface_cercle(rayon);
+	println!(">>> La surface du cercle est de : {}", s);
+
+	let s_sph = calcul::surface_sphere(rayon);
+	println!(">>> La surface de la sphère est de : {}", s_sph);
+
+	let v_sph = calcul::volume_sphere(rayon);
+	println!(">>> Le volume de la sphère est de : {}", v_sph);
+}
